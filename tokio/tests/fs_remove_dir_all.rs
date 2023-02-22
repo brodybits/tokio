0d055068a351412fe119527e9564fa6c42d68d4a
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2023-02-22 15:57:21 -0500

    test updates

--- tokio/tests/fs_remove_dir_all.rs
+++ tokio/tests/fs_remove_dir_all.rs
@@ -8,9 +8,23 @@ use tokio::fs;
 async fn remove_dir_all() {
     let temp_dir = tempdir().unwrap();
 
-    let file_path = temp_dir.path().join("a.txt");
+    let test_dir = temp_dir.path().join("test");
+
+    let file_path = test_dir.path().join("a.txt");
 
     fs::write(&file_path, b"Hello File!").await.unwrap();
 
-    fs::remove_dir_all(temp_dir.path()).await.unwrap();
+    fs::remove_dir_all(test_dir.path()).await.unwrap();
+
+    // test dir should no longer exist
+    match fs::try_exists(test_dir).await {
+        Ok(exists) => assert!(!exists),
+        Err(info) => println!("ignoring error after remove_dir_all, see info: {:?}", info),
+    };
+
+    // contents should no longer exist
+    match fs::try_exists(file_path).await {
+        Ok(exists) => assert!(!exists),
+        Err(info) => println!("ignoring error after remove_dir_all, see info: {:?}", info),
+    };
 }
